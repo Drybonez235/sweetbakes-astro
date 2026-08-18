7d112de941da0be2291d423a6634092052b1d4ce
Jonathan Lewis <jonathan.lewis@welch.edu>
2026-08-18 14:26:54 -0500

    added fall images and changed CTA tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 import tailwindcss from "@tailwindcss/vite";
-// https://astro.build/config
+
+
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
